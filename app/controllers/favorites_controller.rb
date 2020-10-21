e507bad0763fd05cf6529637e0d343631b576875
kuro.katsu <krskthr1229@gmail.com>
2020-10-21 11:54:22 +0000

    favorite, unfavorite

---
+++ app/controllers/favorites_controller.rb
@@ -0,0 +1,17 @@
+class FavoritesController < ApplicationController
+  before_action :require_user_logged_in
+  
+  def create
+    user = User.find(params[:micropost_id])
+    current_user.like(user)
+    flash[:success] = 'お気に入りに追加しました。'
+    redirect_to user
+  end
+
+  def destroy
+    user = User.find(params[:micropost_id])
+    current_user.unlike(user)
+    flash[:success] = 'お気に入りから削除しました。'
+    redirect_to user
+  end
+end
